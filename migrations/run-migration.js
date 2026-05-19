f446d0a2108b4632d1596247e456a84517846c4f
Muhammad Ilham Ridzuan <146695505+ilhamridzuan@users.noreply.github.com>
2026-05-20 01:47:16 +0700

    Update database password environment variable

--- migrations/run-migration.js
+++ migrations/run-migration.js
@@ -457,7 +457,7 @@ async function main() {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       port: process.env.DB_PORT,
-      password: process.env.DB_PASS,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     });
     
